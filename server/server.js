fc9b6260217363c33ed8acc774b2dd82a09c045e
HideOnShroud <natroshviliguga5@gmail.com>
2024-02-17 15:08:08 +0400

    added routes

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRouters = require('./routes/workouts')
 
 // express app
 const app = express()
 
 
 // routes
-app.get('/', (req, res) => {
-    res.json({ mssg: "welcome" })
-})
+app.use('/api/workouts', workoutRouters)
 
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
